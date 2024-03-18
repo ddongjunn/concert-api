a4ed8a224b6541f06657e9e2973a0a8451276466
djlee <ddongjunn@gmail.com>
2024-03-18 17:07:56 +0900

    feat : 포인트 내역 조회 기능 개발

--- hhplus-tdd-jvm-java/src/main/java/io/hhplus/tdd/point/PointService.java
+++ hhplus-tdd-jvm-java/src/main/java/io/hhplus/tdd/point/PointService.java
@@ -4,6 +4,8 @@ import io.hhplus.tdd.database.PointHistoryTable;
 import io.hhplus.tdd.database.UserPointTable;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class PointService {
 
@@ -15,12 +17,17 @@ public class PointService {
         this.pointHistoryTable = pointHistoryTable;
     }
 
-    public UserPoint pointCharge(Long userId, Long amount) throws InterruptedException {
+    public UserPoint chargePoint(Long userId, Long amount) throws InterruptedException {
         UserPoint userPoint = userPointTable.selectById(userId);
+        pointHistoryTable.insert(userId, amount, TransactionType.CHARGE, System.currentTimeMillis());
         return userPointTable.insertOrUpdate(userPoint.id(), userPoint.point() + amount);
     }
 
-    public UserPoint pointCheck(Long userId) throws InterruptedException {
+    public UserPoint checkPoint(Long userId) throws InterruptedException {
         return userPointTable.selectById(userId);
+    }
+
+    public List<PointHistory> checkPointHistory(Long userId){
+        return pointHistoryTable.selectAllByUserId(userId);
     }
 }
--- hhplus-tdd-jvm-java/src/test/java/io/hhplus/tdd/point/PointServiceTest.java
+++ hhplus-tdd-jvm-java/src/test/java/io/hhplus/tdd/point/PointServiceTest.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
@@ -33,7 +35,7 @@ public class PointServiceTest {
         Long amount = 500L;
 
         // When
-        UserPoint userPoint = pointService.pointCharge(userId, amount);
+        UserPoint userPoint = pointService.chargePoint(userId, amount);
 
         // Then
         UserPoint result = new UserPoint(1L, 500L, System.currentTimeMillis());
@@ -42,15 +44,15 @@ public class PointServiceTest {
     }
 
     @Test
-    @DisplayName("[포인트 추가 충전] - 포인트가 이미 존재 하는 사용자의 포인트 충전 ")
+    @DisplayName("[포인트 추가 충전] - 포인트가 존재하는 사용자의 포인트 충전")
     public void GivenUserIdAndAmount_WhenRechargingPoint_ThenUserPoint() throws InterruptedException {
         // Given
         Long userId = 1L;
         Long amount = 500L;
 
         // When
-        pointService.pointCharge(userId, amount);
-        UserPoint rechargeUserPoint = pointService.pointCharge(userId, 500L);
+        pointService.chargePoint(userId, amount);
+        UserPoint rechargeUserPoint = pointService.chargePoint(userId, 500L);
 
         // Then
         UserPoint result = new UserPoint(userId, 1000L, rechargeUserPoint.updateMillis());
@@ -59,15 +61,15 @@ public class PointServiceTest {
     }
 
     @Test
-    @DisplayName("[포인트 조회] - 포인트가 이미 존재하는 사용자 포인트 조회")
-    public void GivenUserIdAndAmount_WhenCheckingPoint_ThenUserPoint() throws InterruptedException {
+    @DisplayName("[포인트 조회] - 포인트가 존재하는 사용자 포인트 조회")
+    public void GivenUserId_WhenCheckingPoint_ThenUserPoint() throws InterruptedException {
         //Given
         Long userId = 1L;
         Long amount = 1000L;
-        UserPoint userPoint = pointService.pointCharge(userId, amount);
+        UserPoint userPoint = pointService.chargePoint(userId, amount);
 
         // When
-        UserPoint result = pointService.pointCheck(userId);
+        UserPoint result = pointService.checkPoint(userId);
 
         // Then
         assertThat(userPoint.id()).isEqualTo(result.id());
@@ -75,17 +77,52 @@ public class PointServiceTest {
     }
 
     @Test
-    @DisplayName("[포인트 조회] - 포인트가 이미 존재 하지 않는 사용자 포인트 조회")
-    public void GivenUserIdAndAmount_WhenCheckingPoint_ThenNoPointExist() throws InterruptedException {
+    @DisplayName("[포인트 조회] - 포인트가 존재 하지 않는 사용자 포인트 조회")
+    public void GivenUserId_WhenCheckingPoint_ThenNoPointExist() throws InterruptedException {
         //Given
         Long userId = 1L;
         UserPoint userPoint = new UserPoint(userId, 0L, System.currentTimeMillis());
 
         // When
-        UserPoint result = pointService.pointCheck(userId);
+        UserPoint result = pointService.checkPoint(userId);
 
         // Then
         assertThat(userPoint.id()).isEqualTo(result.id());
         assertThat(userPoint.point()).isEqualTo(result.point());
+    }
+
+    @Test
+    @DisplayName("[포인트 내역 조회] - 포인트가 존재 하는 사용자 포인트 내역 조회")
+    public void GivenUserId_WhenCheckingPointHistories_ThenNoPointHistoriesExist() throws InterruptedException {
+        //Given
+        Long userId = 1L;
+        Long amount = 1000L;
+        pointService.chargePoint(userId, amount);
+
+        // When
+        List<PointHistory> pointHistories = pointService.checkPointHistory(userId);
+
+        // Then
+        PointHistory result = new PointHistory(1L, userId, TransactionType.CHARGE, 1000L, System.currentTimeMillis());
+        assertThat(pointHistories).hasSize(1);
+
+        PointHistory firstPointHistory = pointHistories.get(0);
+        assertThat(firstPointHistory.id()).isEqualTo(result.id());
+        assertThat(firstPointHistory.userId()).isEqualTo(result.userId());
+        assertThat(firstPointHistory.type()).isEqualTo(result.type());
+        assertThat(firstPointHistory.amount()).isEqualTo(result.amount());
+    }
+
+    @Test
+    @DisplayName("[포인트 내역 조회] - 포인트가 존재 하지 않는 사용자 포인트 내역 조회")
+    public void GivenUserId_WhenCheckingPointHistories_ThenPointHistoriesExist() throws InterruptedException {
+        //Given
+        Long userId = 1L;
+
+        // When
+        List<PointHistory> pointHistories = pointService.checkPointHistory(userId);
+
+        // Then
+        assertThat(pointHistories).hasSize(0);
     }
 }
