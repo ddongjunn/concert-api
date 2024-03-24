1635d11a1dcf6ccbdee32f8ca23a05e4c5402840
djlee <ddongjunn@gmail.com>
2024-03-24 23:52:48 +0900

    feat : command에 대한 입력 유효성 검증 추가

--- hhplus-clean-architecture/src/main/java/com/hhplus/api/lecture/application/port/in/ApplyLectureCommand.java
+++ hhplus-clean-architecture/src/main/java/com/hhplus/api/lecture/application/port/in/ApplyLectureCommand.java
@@ -1,2 +1,23 @@
-package com.hhplus.api.lecture.application.port.in;public class ApplyLectureCommand {
+package com.hhplus.api.lecture.application.port.in;
+
+import com.hhplus.api.common.SelfValidating;
+import jakarta.validation.constraints.NotNull;
+import lombok.EqualsAndHashCode;
+import lombok.Value;
+
+@Value
+@EqualsAndHashCode(callSuper = false)
+public class ApplyLectureCommand extends SelfValidating<ApplyLectureCommand> {
+
+    @NotNull Long lectureId;
+
+    @NotNull Long userId;
+
+    public ApplyLectureCommand(
+            Long lectureId,
+            Long userId){
+        this.lectureId = lectureId;
+        this.userId = userId;
+        this.validateSelf();
+    }
 }
