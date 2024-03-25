959ef54e28d1e75f66e352026f8ce64a8629d193
ddongjunn <ddongjunn@gmail.com>
2024-03-25 18:00:02 +0900

    feat : Lecture 도메인 필드 수정
    현재 신청한 인원, 신청 가능한 인원 추가

--- hhplus-clean-architecture/src/main/java/com/hhplus/api/lecture/domain/Lecture.java
+++ hhplus-clean-architecture/src/main/java/com/hhplus/api/lecture/domain/Lecture.java
@@ -2,13 +2,28 @@ package com.hhplus.api.lecture.domain;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 import java.time.LocalDateTime;
 
+@Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class Lecture {
     Long id;
     String name;
-    int currentAttendees;
+    int applicantCount;
+    int capacityLimit;
     LocalDateTime startDate;
+
+    public static Lecture of(Long id, String name, int applicantCount, int capacityLimit, LocalDateTime startDate) {
+        return new Lecture(id, name, applicantCount, capacityLimit, startDate);
+    }
+
+    public boolean isApplicationPossible(){
+        return this.applicantCount < this.capacityLimit;
+    }
+
+    public void incrementApplicantCount(){
+        this.applicantCount++;
+    }
 }
