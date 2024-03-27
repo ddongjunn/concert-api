a7923d7031d3d9d26c0497d3afbd7639e2a15c2d
djlee <ddongjunn@gmail.com>
2024-03-27 23:45:48 +0900

    feat : 리턴 메세지 추가

--- hhplus-clean-architecture/src/main/java/com/hhplus/api/common/enums/ReturnMessage.java
+++ hhplus-clean-architecture/src/main/java/com/hhplus/api/common/enums/ReturnMessage.java
@@ -1,12 +1,14 @@
 package com.hhplus.api.common.enums;
 
+import com.hhplus.api.lecture.application.exception.LectureApplicationFullException;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
 public enum ReturnMessage {
-    ALREADY_SIGNED_UP_FOR_LECTURE("이미 신청한 강의 입니다.");
+    ALREADY_SIGNED_UP_FOR_LECTURE("이미 신청한 강의 입니다."),
+    LECTURE_FULL("정원이 가득 찼습니다.");
 
     private final String message;
 }
