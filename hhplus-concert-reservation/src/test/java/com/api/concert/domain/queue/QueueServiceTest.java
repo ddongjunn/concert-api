1b8ba3023e69025cc2094ff544f48066e771f0bf
ddongjunn <ddongjunn@gmail.com>
2024-04-11 17:50:39 +0900

    feat : 대기열 등록 동시성 테스트
    현재 동일한 사용자가 여러번 신청할 경우 테스트 통과하지 못함

--- hhplus-concert-reservation/src/test/java/com/api/concert/domain/queue/QueueServiceTest.java
+++ hhplus-concert-reservation/src/test/java/com/api/concert/domain/queue/QueueServiceTest.java
@@ -1,12 +1,15 @@
 package com.api.concert.domain.queue;
 
-import com.api.concert.controller.queue.dto.QueueRequest;
+import com.api.concert.controller.queue.dto.QueueRegisterRequest;
+import com.api.concert.infrastructure.queue.QueueEntity;
+import com.api.concert.infrastructure.queue.QueueJpaRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -22,18 +25,52 @@ public class QueueServiceTest {
     private final QueueOption queueOption;
     private final IQueueRepository iQueueRepository;
 
+    private final QueueJpaRepository queueJpaRepository;
+
     @Autowired
-    public QueueServiceTest(QueueService queueService, QueueOption queueOption, IQueueRepository iQueueRepository) {
+    public QueueServiceTest(QueueService queueService, QueueOption queueOption, IQueueRepository iQueueRepository, QueueJpaRepository queueJpaRepository) {
         this.queueService = queueService;
         this.queueOption = queueOption;
         this.iQueueRepository = iQueueRepository;
+        this.queueJpaRepository = queueJpaRepository;
+    }
+
+    @DisplayName("한명의 사용자가 동시에 신청하는 경우")
+    @Test
+    void test_register_onlyOne() throws InterruptedException{
+        //Given
+        QueueRegisterRequest queueRegisterRequest = QueueRegisterRequest.builder().userId(1L).build();
+
+        //When
+        int numberOfRequests = 10;
+        CountDownLatch latch = new CountDownLatch(numberOfRequests);
+        ExecutorService executorService = Executors.newFixedThreadPool(numberOfRequests);
+
+        for(int i = 0; i < numberOfRequests; i++){
+            executorService.submit(() -> {
+                try {
+                    queueService.register(queueRegisterRequest);
+                } catch (Exception e) {
+                    log.error(e.getMessage());
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+        latch.await();
+
+        //Then
+        long countOfOngoingStatus = iQueueRepository.getCountOfOngoingStatus();
+        List<QueueEntity> all = queueJpaRepository.findAll();
+        all.forEach(a -> log.info("list : {}", a.toString()));
+        assertThat(countOfOngoingStatus).isEqualTo(1L);
     }
 
     @DisplayName("동시에 여러명이 대기열 등록")
     @Test
     void test_register() throws InterruptedException{
         //Given
-        QueueRequest queueRequest = QueueRequest.builder().userId(1L).build();
+        QueueRegisterRequest queueRegisterRequest = QueueRegisterRequest.builder().userId(1L).build();
 
         //When
         int numberOfRequests = 100;
@@ -44,7 +81,7 @@ public class QueueServiceTest {
             Long userId = (long) i;
             executorService.submit(() -> {
                 try {
-                    queueService.register(QueueRequest.builder().userId(userId).build());
+                    queueService.register(QueueRegisterRequest.builder().userId(userId).build());
                 } catch (Exception e) {
                     log.error(e.getMessage());
                 } finally {
@@ -58,6 +95,5 @@ public class QueueServiceTest {
         long countOfOngoingStatus = iQueueRepository.getCountOfOngoingStatus();
         assertThat(countOfOngoingStatus).isEqualTo(QUEUE_LIMIT);
     }
-
 
 }
