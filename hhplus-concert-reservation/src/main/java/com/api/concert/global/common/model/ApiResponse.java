83aad11a9a3f59eef6369543b30ff411c1f95278
djlee <ddongjunn@gmail.com>
2024-04-12 22:52:53 +0900

    feat : 공통 Api response

---
+++ hhplus-concert-reservation/src/main/java/com/api/concert/global/common/model/ApiResponse.java
@@ -0,0 +1,21 @@
+package com.api.concert.global.common.model;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+@Getter
+public class ApiResponse<T> {
+
+    private final LocalDateTime timestamp = LocalDateTime.now();
+    private String path;
+    private T data;
+    private String message;
+
+    @Builder
+    private ApiResponse(String path, T data, String message) {
+        this.path = path;
+        this.data = data;
+        this.message = message;
+    }
+}
