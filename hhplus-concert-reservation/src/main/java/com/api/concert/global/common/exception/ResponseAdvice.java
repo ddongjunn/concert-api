eb3e11ec269e6026547626e34b97c80e06012fdd
djlee <ddongjunn@gmail.com>
2024-04-14 00:49:19 +0900

    fix : 불필요한 import 제거

--- hhplus-concert-reservation/src/main/java/com/api/concert/global/common/exception/ResponseAdvice.java
+++ hhplus-concert-reservation/src/main/java/com/api/concert/global/common/exception/ResponseAdvice.java
@@ -4,7 +4,6 @@ import com.api.concert.global.common.model.ApiResponse;
 import org.springframework.core.MethodParameter;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
