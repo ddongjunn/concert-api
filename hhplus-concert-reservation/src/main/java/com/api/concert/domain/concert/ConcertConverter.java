3bdea5989594e8b1d3aa66a53551405ed90c0d23
djlee <ddongjunn@gmail.com>
2024-04-14 00:51:16 +0900

    fix : 불필요한 클래스 제거

--- hhplus-concert-reservation/src/main/java/com/api/concert/domain/concert/ConcertConverter.java
+++ hhplus-concert-reservation/src/main/java/com/api/concert/domain/concert/ConcertConverter.java
@@ -1,22 +1,4 @@
 package com.api.concert.domain.concert;
 
-import com.api.concert.controller.concert.dto.ConcertResponseDto;
-import com.api.concert.infrastructure.concert.projection.ConcertInfo;
-
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class ConcertConverter {
-    public static ConcertResponseDto toResponse(List<ConcertInfo> concerts){
-        List<ConcertResponseDto.ConcertDto> concertDto = concerts.stream()
-                .map(concert -> ConcertResponseDto.ConcertDto.builder()
-                        .concertOptionId(concert.getConcertOptionId())
-                        .name(concert.getName())
-                        .singer(concert.getSinger())
-                        .venue(concert.getVenue())
-                        .startDate(concert.getStartDate())
-                        .build())
-                .collect(Collectors.toList());
-        return ConcertResponseDto.builder().concerts(concertDto).build();
-    }
 }
