783d59fca25112a8bfa27cfe3ce8b70b943590e8
djlee <ddongjunn@gmail.com>
2024-04-15 00:08:04 +0900

    test : 예약 가능한 콘서트 조회 단위 테스트 추가

--- hhplus-concert-reservation/src/test/java/com/api/concert/domain/concert/ConcertServiceTest.java
+++ hhplus-concert-reservation/src/test/java/com/api/concert/domain/concert/ConcertServiceTest.java
@@ -1,5 +1,6 @@
 package com.api.concert.domain.concert;
 
+import com.api.concert.infrastructure.concert.projection.ConcertInfo;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -7,11 +8,13 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.anyList;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class ConcertServiceTest {
@@ -24,14 +27,20 @@ class ConcertServiceTest {
 
     @DisplayName("[콘서트 조회]")
     @Test
-    void test_findAvailableConcerts(){
+    void test_findAvailableConcerts() {
         // Given
+        ConcertInfo concertOne = mock(ConcertInfo.class);
+        ConcertInfo concertTwo = mock(ConcertInfo.class);
+        List<ConcertInfo> mockConcerts = Arrays.asList(concertOne, concertTwo);
 
         // When
-        //when(iConcertRepository.findAvailableConcerts()).thenReturn(anyList());
+        when(iConcertRepository.availableConcerts()).thenReturn(mockConcerts);
 
         // Then
-        List<Concert> availableConcerts = concertService.findAvailableConcerts();
-        assertThat(availableConcerts).isEmpty();
+        List<ConcertInfo> result = concertService.getAvailableConcerts();
+        assertThat(result).isNotEmpty();
+        assertThat(result).hasSize(2);
+        verify(iConcertRepository, times(1)).availableConcerts();
     }
+
 }
