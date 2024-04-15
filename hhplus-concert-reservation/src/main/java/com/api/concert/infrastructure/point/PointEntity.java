737b2465d8ad2426c5575f6f9f2b7faf7146b295
djlee <ddongjunn@gmail.com>
2024-04-15 19:45:44 +0900

    fix : add builder

--- hhplus-concert-reservation/src/main/java/com/api/concert/infrastructure/point/PointEntity.java
+++ hhplus-concert-reservation/src/main/java/com/api/concert/infrastructure/point/PointEntity.java
@@ -2,10 +2,14 @@ package com.api.concert.infrastructure.point;
 
 import com.api.concert.global.entity.BaseEntity;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Getter
 @Entity
 @Table(name = "point")
