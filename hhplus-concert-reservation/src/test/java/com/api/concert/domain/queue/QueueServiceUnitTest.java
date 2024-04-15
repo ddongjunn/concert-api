6198e693de1c7bfa4790582c5f547d0f477ebaed
djlee <ddongjunn@gmail.com>
2024-04-15 19:44:36 +0900

    test : 불필요한 import 제거

--- hhplus-concert-reservation/src/test/java/com/api/concert/domain/queue/QueueServiceUnitTest.java
+++ hhplus-concert-reservation/src/test/java/com/api/concert/domain/queue/QueueServiceUnitTest.java
@@ -23,7 +23,6 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
