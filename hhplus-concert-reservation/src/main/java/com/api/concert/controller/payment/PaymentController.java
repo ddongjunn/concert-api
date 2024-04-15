f8e50d4cc7bee38ce2b6614a6a1a239397d88eb4
djlee <ddongjunn@gmail.com>
2024-04-15 19:43:36 +0900

    fix : point 분리

--- hhplus-concert-reservation/src/main/java/com/api/concert/controller/payment/PaymentController.java
+++ hhplus-concert-reservation/src/main/java/com/api/concert/controller/payment/PaymentController.java
@@ -5,27 +5,6 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class PaymentController {
 
-    @PatchMapping("/point")
-    public String patchPoint(){
-        return """
-                {
-                  "status": "SUCCESS",
-                  "charge_point": 1000,
-                  "point": 5000
-                }
-                """;
-    }
-
-    @GetMapping("/point/{userId}")
-    public String getPoint(@PathVariable Long userId){
-        return """
-                {
-                  "status": "SUCCESS",
-                  "point": 2000
-                }
-                """;
-    }
-
     @PostMapping("/concert/payment")
     public String concertPayment(){
         return """
