48c73cbf3126d6cab5b073ec6b9c4ff3c18bd671
ddongjunn <ddongjunn@gmail.com>
2024-04-16 17:46:37 +0900

    test : 좌석 조회 테스트 삭제
    좌석 조회 테스트는 ConcertSeatServiceTest로 옮김

--- hhplus-concert-reservation/src/test/java/com/api/concert/domain/concert/ConcertServiceTest.java
+++ hhplus-concert-reservation/src/test/java/com/api/concert/domain/concert/ConcertServiceTest.java
@@ -1,5 +1,7 @@
 package com.api.concert.domain.concert;
 
+import com.api.concert.global.common.exception.CommonException;
+import com.api.concert.global.common.model.ResponseCode;
 import com.api.concert.infrastructure.concert.projection.ConcertInfo;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -8,19 +10,16 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.time.LocalDateTime;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.anyList;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class ConcertServiceTest {
-
-    @Mock
-    IConcertRepository iConcertRepository;
 
     @Mock
     IConcertOptionRepository iConcertOptionRepository;
@@ -32,30 +31,37 @@ class ConcertServiceTest {
     @Test
     void test_findAvailableConcerts() {
         // Given
-        ConcertInfo concertOne = mock(ConcertInfo.class);
-        ConcertInfo concertTwo = mock(ConcertInfo.class);
-        List<ConcertInfo> mockConcerts = Arrays.asList(concertOne, concertTwo);
+        List<ConcertInfo> mockConcerts = makeReservedConcerts(5);
 
         // When
         when(iConcertOptionRepository.availableConcerts()).thenReturn(mockConcerts);
 
         // Then
-        List<ConcertInfo> result = concertService.getAvailableConcerts();
+        List<ConcertInfo> result = concertService.findAvailableConcerts();
         assertThat(result).isNotEmpty();
-        assertThat(result).hasSize(2);
+        assertThat(result).hasSize(5);
         verify(iConcertOptionRepository, times(1)).availableConcerts();
     }
 
-    @DisplayName("예약 가능한 콘서트 좌석 조회")
+    @DisplayName("예약 가능한 콘서트가 없는 경우")
     @Test
-    void test_findAvailableConcertSeat(){
+    void test_findAvailableConcerts_notExist() {
         // Given
-        Long concertOptionId = 1L;
+        List<ConcertInfo> mockConcerts = Collections.emptyList();
 
-        // When
-        concertService.findAvailableConcertSeat(concertOptionId);
+        // When & Then
+        when(iConcertOptionRepository.availableConcerts()).thenReturn(mockConcerts);
 
-        // Then
+        assertThatThrownBy(() -> concertService.findAvailableConcerts())
+                .isInstanceOf(CommonException.class)
+                .hasFieldOrPropertyWithValue("ResponseCode", ResponseCode.NO_CONCERT_AVAILABLE)
+                .hasMessage(ResponseCode.NO_CONCERT_AVAILABLE.getMessage());
+    }
+
+    List<ConcertInfo> makeReservedConcerts(int size){
+        return IntStream.rangeClosed(1, size)
+                .mapToObj(i -> mock(ConcertInfo.class))
+                .toList();
     }
 
 }
