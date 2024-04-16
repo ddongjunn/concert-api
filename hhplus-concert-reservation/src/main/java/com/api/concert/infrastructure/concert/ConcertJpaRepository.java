438c2ea463904948efecc77563f6e5559475ee29
ddongjunn <ddongjunn@gmail.com>
2024-04-16 17:45:45 +0900

    fix : 불필요한 import 제거

--- hhplus-concert-reservation/src/main/java/com/api/concert/infrastructure/concert/ConcertJpaRepository.java
+++ hhplus-concert-reservation/src/main/java/com/api/concert/infrastructure/concert/ConcertJpaRepository.java
@@ -1,10 +1,6 @@
 package com.api.concert.infrastructure.concert;
 
-import com.api.concert.infrastructure.concert.projection.ConcertInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import java.util.List;
 
 public interface ConcertJpaRepository extends JpaRepository<ConcertEntity, Long> {
 
