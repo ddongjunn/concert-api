8cb2bafca0087323dbcc0e4eba09d0d75107d592
ddongjunn <ddongjunn@gmail.com>
2024-04-17 17:45:22 +0900

    feat : 예약 히스토리 저장 추가

--- hhplus-concert-reservation/src/main/java/com/api/concert/domain/concert/IReservationRepository.java
+++ hhplus-concert-reservation/src/main/java/com/api/concert/domain/concert/IReservationRepository.java
@@ -1,4 +1,7 @@
 package com.api.concert.domain.concert;
 
+import com.api.concert.infrastructure.concert.ReservationEntity;
+
 public interface IReservationRepository {
+    void save(ReservationEntity entity);
 }
