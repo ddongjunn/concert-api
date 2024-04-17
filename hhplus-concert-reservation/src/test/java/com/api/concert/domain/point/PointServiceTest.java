a1064fc7c0ecd8a760fc0488f491f68d4b3a3b18
djlee <ddongjunn@gmail.com>
2024-04-18 00:27:59 +0900

    test : 포인트 사용 단위테스트 추가

--- hhplus-concert-reservation/src/test/java/com/api/concert/domain/point/PointServiceTest.java
+++ hhplus-concert-reservation/src/test/java/com/api/concert/domain/point/PointServiceTest.java
@@ -2,7 +2,9 @@ package com.api.concert.domain.point;
 
 import com.api.concert.controller.point.dto.PointChargeRequest;
 import com.api.concert.controller.point.dto.PointChargeResponse;
+import com.api.concert.controller.point.dto.PointUseRequest;
 import com.api.concert.global.common.exception.CommonException;
+import com.api.concert.global.common.exception.InsufficientPointsException;
 import com.api.concert.global.common.model.ResponseCode;
 import com.api.concert.infrastructure.point.PointEntity;
 import org.junit.jupiter.api.DisplayName;
@@ -16,7 +18,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class PointServiceTest {
@@ -46,11 +48,11 @@ class PointServiceTest {
         assertThat(result.getPoint()).isEqualTo(point.getPoint());
     }
 
-    @DisplayName("포인트 충전")
+    @DisplayName("[포인트 충전] - 정상")
     @Test
     void test_charge(){
         // Given
-        PointChargeRequest pointChargeRequest = createPointRequest(1L, 500L);
+        PointChargeRequest pointChargeRequest = createPointChargeRequest(1L, 500L);
         Long userId = pointChargeRequest.userId();
         Long chargePoint = pointChargeRequest.point();
 
@@ -61,7 +63,7 @@ class PointServiceTest {
         Point pointCharge = Point.builder().userId(userId).point(findPoint.getPoint() + chargePoint).build();
         when(iPointRepository.updatePoint(any(PointEntity.class))).thenReturn(pointCharge);
         findPoint.charge(chargePoint, pointHistory -> {
-            pointService.saveHistory(pointHistory);
+
         });
 
         // Then
@@ -70,11 +72,11 @@ class PointServiceTest {
         assertThat(result.point()).isEqualTo(1500L);
     }
 
-    @DisplayName("포인트 음수 충전")
+    @DisplayName("[포인트 충전] - 음수")
     @Test
     void test_charge_negative(){
         // Given
-        PointChargeRequest pointChargeRequest = createPointRequest(1L, -500L);
+        PointChargeRequest pointChargeRequest = createPointChargeRequest(1L, -500L);
         Long userId = pointChargeRequest.userId();
         Long chargePoint = pointChargeRequest.point();
 
@@ -87,11 +89,62 @@ class PointServiceTest {
                 .isInstanceOf(CommonException.class)
                 .hasFieldOrPropertyWithValue("ResponseCode", ResponseCode.POINT_CHARGE_NEGATIVE)
                 .hasMessage(ResponseCode.POINT_CHARGE_NEGATIVE.getMessage());
-
     }
 
-    PointChargeRequest createPointRequest(Long userId, Long point){
+    @DisplayName("[포인트 사용] - 포인트가 부족할 경우")
+    @Test
+    void test_use_lack(){
+        // Given
+        Long userId = 1L;
+        Long usePoint = 1000L;
+        PointUseRequest pointUseRequest = createPointUseRequest(userId, usePoint);
+
+        // When
+        Point point = Point.builder().pointId(1L).userId(1L).point(0L).build();
+        when(iPointRepository.findPointByUserId(userId)).thenReturn(point);
+
+        // Then
+        assertThatThrownBy(() -> pointService.use(pointUseRequest))
+                .isInstanceOf(InsufficientPointsException.class)
+                .hasFieldOrPropertyWithValue("code", ResponseCode.NOT_ENOUGH_POINT)
+                .hasFieldOrPropertyWithValue("insufficientPoint", 1000L)
+                .hasFieldOrPropertyWithValue("currentPoint", 0L)
+                .hasMessage(ResponseCode.NOT_ENOUGH_POINT.getMessage());
+    }
+
+    @DisplayName("[포인트 사용] - 정상")
+    @Test
+    void test_use(){
+        // Given
+        Long userId = 1L;
+        Long usePoint = 1000L;
+        PointUseRequest pointUseRequest = createPointUseRequest(userId, usePoint);
+
+        // When
+        Point findPoint = Point.builder().pointId(1L).userId(1L).point(2000L).build();
+        when(iPointRepository.findPointByUserId(userId)).thenReturn(findPoint);
+
+        Point updatePoint = Point.builder().pointId(1L).userId(1L).point(1000L).build();
+        when(iPointRepository.updatePoint(any(PointEntity.class))).thenReturn(updatePoint);
+        findPoint.use(usePoint, pointHistory -> {});
+
+        PointChargeResponse result = pointService.use(pointUseRequest);
+
+        // Then
+        verify(iPointRepository, times(1)).updatePoint(any(PointEntity.class));
+        assertThat(result.point()).isEqualTo(1000);
+    }
+
+
+    PointChargeRequest createPointChargeRequest(Long userId, Long point){
         return PointChargeRequest.builder()
+                .userId(userId)
+                .point(point)
+                .build();
+    }
+
+    PointUseRequest createPointUseRequest(Long userId, Long point){
+        return PointUseRequest.builder()
                 .userId(userId)
                 .point(point)
                 .build();
--- hhplus-concert-reservation/src/test/java/com/api/concert/domain/point/PointTest.java
+++ hhplus-concert-reservation/src/test/java/com/api/concert/domain/point/PointTest.java
@@ -14,7 +14,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @ExtendWith(MockitoExtension.class)
-class PointUnitTest {
+class PointTest {
 
     private Point point;
 
