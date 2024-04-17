faaff30168f69492220312647ed460906c64ba10
djlee <ddongjunn@gmail.com>
2024-04-17 23:49:48 +0900

    test : 콘서트 좌석 단위 테스트 추가

--- hhplus-concert-reservation/src/test/java/com/api/concert/domain/concert/ConcertSeatServiceTest.java
+++ hhplus-concert-reservation/src/test/java/com/api/concert/domain/concert/ConcertSeatServiceTest.java
@@ -7,38 +7,48 @@ import com.api.concert.domain.concert.constant.SeatStatus;
 import com.api.concert.global.common.exception.CommonException;
 import com.api.concert.global.common.model.ResponseCode;
 import com.api.concert.infrastructure.concert.ConcertSeatEntity;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.awt.print.Pageable;
+import java.lang.reflect.Field;
 import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.LongStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.assertj.core.api.BDDAssertions.then;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class ConcertSeatServiceTest {
 
-    private final int SEAT_TEMP_TIME = 1;
+    private int SEAT_LIMIT;
+    private int SEAT_TEMP_TIME;
 
     @Mock
     IConcertSeatRepository iConcertSeatRepository;
 
     @InjectMocks
     ConcertSeatService concertSeatService;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        Field seatLimitField = ConcertSeatService.class.getDeclaredField("SEAT_LIMIT");
+        seatLimitField.setAccessible(true);
+        this.SEAT_LIMIT = (int) seatLimitField.get(concertSeatService);
+
+        Field seatTempTimeField = ConcertSeatService.class.getDeclaredField("SEAT_TEMP_TIME");
+        seatTempTimeField.setAccessible(true);
+        this.SEAT_TEMP_TIME = (int) seatTempTimeField.get(concertSeatService);
+    }
 
     @DisplayName("예약 가능한 좌석 조회")
     @Test
@@ -49,7 +59,7 @@ class ConcertSeatServiceTest {
 
         // When
         when(iConcertSeatRepository.findReservedSeats(any())).thenReturn(reservedSeats);
-        ConcertSeatResponse availableConcertSeat = concertSeatService.findAvailableConcertSeat(concertOptionId);
+        ConcertSeatResponse availableConcertSeat = concertSeatService.findAvailableSeatsForConcert(concertOptionId);
 
         // Then
         assertThat(availableConcertSeat.seats()).hasSize(44);
@@ -64,10 +74,40 @@ class ConcertSeatServiceTest {
 
         // When & Then
         when(iConcertSeatRepository.findReservedSeats(any())).thenReturn(reservedSeats);
-        assertThatThrownBy(() -> concertSeatService.findAvailableConcertSeat(concertOptionId))
+        assertThatThrownBy(() -> concertSeatService.findAvailableSeatsForConcert(concertOptionId))
                 .isInstanceOf(CommonException.class)
                 .hasFieldOrPropertyWithValue("ResponseCode", ResponseCode.NO_SEATS_AVAILABLE)
                 .hasMessage(ResponseCode.NO_SEATS_AVAILABLE.getMessage());
+    }
+
+    @DisplayName("예약 가능한 콘서트 좌석의 개수가 SEAT_LIMIT 같은 경우 Exception")
+    @Test
+    void test_assertSeatsAvailable(){
+        // Given
+        List<ConcertSeat> reservedSeats = makeReservedSeats(SEAT_LIMIT);
+
+        // When & Then
+        assertThatThrownBy(() -> concertSeatService.assertSeatsAvailable(reservedSeats))
+                .isInstanceOf(CommonException.class)
+                .hasFieldOrPropertyWithValue("ResponseCode", ResponseCode.NO_SEATS_AVAILABLE)
+                .hasMessage(ResponseCode.NO_SEATS_AVAILABLE.getMessage());
+
+    }
+
+    @DisplayName("예약 가능한 좌석번호는 예약된 좌석번호를 제외하고 생성한다.")
+    @Test
+    void test_calculateAvailableSeats(){
+        // Given
+        List<ConcertSeat> reservedSeats = makeReservedSeats(SEAT_LIMIT - 10);
+
+        // When
+        List<ConcertSeat> availableSeats = concertSeatService.calculateAvailableSeats(reservedSeats, SEAT_LIMIT);
+
+        // Then
+        assertThat(availableSeats).hasSize(10);
+        assertThat(availableSeats)
+                .extracting("seatNo")
+                .containsExactly(41, 42, 43, 44, 45, 46, 47, 48, 49, 50);
     }
 
     @DisplayName("[좌석 임시 예약] - 이미 예약된 좌석인 경우 Exception")
@@ -90,7 +130,7 @@ class ConcertSeatServiceTest {
                 .hasMessage(ResponseCode.ALREADY_RESERVED_SEAT.getMessage());
     }
 
-    @DisplayName("[좌석 임시 예약] - 좌석 정보가 null 인 경우")
+    @DisplayName("[좌석 임시 예약] - 좌석 정보가 null인 경우 임시 예약 상태로 저장한다.")
     @Test
     void test_temporaryReservationSeat_null(){
         // Given
@@ -110,7 +150,7 @@ class ConcertSeatServiceTest {
         assertThat(result.code()).isEqualTo(ResponseCode.SUCCESS);
     }
 
-    @DisplayName("[좌석 임시 예약] - 좌석 정보 상태가 AVAILABLE 경우")
+    @DisplayName("[좌석 임시 예약] - 좌석 정보 상태가 AVAILABLE인 경우 임시 예약 상태로 저장한다.")
     @Test
     void test_temporaryReservationSeat_available(){
         // Given
@@ -128,6 +168,47 @@ class ConcertSeatServiceTest {
         ConcertTempReservationResponse result = concertSeatService.temporaryReservationSeat(request);
         assertThat(result).isNotNull();
         assertThat(result.code()).isEqualTo(ResponseCode.SUCCESS);
+    }
+
+    @DisplayName("좌석을 임시 예약 상태로 생성하여 저장한다.")
+    @Test
+    void test_createConcertSeatForTemporaryReservation(){
+        // Given
+        Long concertOptionId = 1L;
+        Long userId = 1L;
+        int seatNo = 10;
+
+        // When
+        concertSeatService.createConcertSeatForTemporaryReservation(concertOptionId, userId, seatNo);
+        ArgumentCaptor<ConcertSeatEntity> concertSeatArgumentCaptor = ArgumentCaptor.forClass(ConcertSeatEntity.class);
+
+        // Then
+        verify(iConcertSeatRepository, times(1)).save(concertSeatArgumentCaptor.capture());
+        ConcertSeat capturedSeat = ConcertSeatEntity.toDomain(concertSeatArgumentCaptor.getValue());
+
+        assertThat(capturedSeat.getConcertOptionId()).isEqualTo(1);
+        assertThat(capturedSeat.getUserId()).isEqualTo(1);
+        assertThat(capturedSeat.getStatus()).isEqualTo(SeatStatus.TEMPORARY);
+    }
+
+    @DisplayName("좌석을 임시 예약 상태로 수정하여 저장한다.")
+    @Test
+    void test_updateConcertSeatTemporaryReservation(){
+        // Given
+        Long userId = 1L;
+        ConcertSeat concertSeat = ConcertSeat.builder().concertOptionId(1L).userId(null).status(SeatStatus.AVAILABLE).build();
+
+        // When
+        concertSeatService.updateConcertSeatTemporaryReservation(concertSeat, userId);
+        ArgumentCaptor<ConcertSeatEntity> concertSeatArgumentCaptor = ArgumentCaptor.forClass(ConcertSeatEntity.class);
+
+        // Then
+        verify(iConcertSeatRepository, times(1)).save(concertSeatArgumentCaptor.capture());
+        ConcertSeat capturedSeat = ConcertSeatEntity.toDomain(concertSeatArgumentCaptor.getValue());
+
+        assertThat(capturedSeat.getConcertOptionId()).isEqualTo(1);
+        assertThat(capturedSeat.getUserId()).isEqualTo(1);
+        assertThat(capturedSeat.getStatus()).isEqualTo(SeatStatus.TEMPORARY);
     }
 
     @DisplayName("존재 하지 않는 좌석번호")
