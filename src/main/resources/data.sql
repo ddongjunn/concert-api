62c150c62ea940b385d77af3f3243168b59b7d07
ddongjunn <ddongjunn@gmail.com>
2024-05-23 17:42:56 +0900

    chore : init data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,14 +3,14 @@ insert into concert (concert_id, name, singer, created_at, updated_at) values(2,
 insert into concert (concert_id, name, singer, created_at, updated_at) values(3, '황영웅 대전콘서트 〈봄날의 고백>', '황영웅', now(), now());
 insert into concert (concert_id, name, singer, created_at, updated_at) values(4, '김태우 2024 T-PARTY < ROOM 512 >', '김태우', now(), now());
 
-insert into concert_option (concert_option_id, concert_id, venue, reservation_start_date, start_date, created_at, updated_at) values (1, 1, '세종예술의전당', '2024-04-10T00:00:00', '2024-04-15T00:00:00', now(), now());
-insert into concert_option (concert_option_id, concert_id, venue, reservation_start_date, start_date, created_at, updated_at) values (2, 1, '건국대학교 새천년관 대공연장', '2024-04-11T00:00:00', '2024-04-16T00:00:00', now(), now());
-insert into concert_option (concert_option_id, concert_id, venue, reservation_start_date, start_date, created_at, updated_at) values (3, 1, '컨벤션센터 2전시장', '2024-04-08T00:00:00', '2024-04-10T00:00:00', now(), now());
-insert into concert_option (concert_option_id, concert_id, venue, reservation_start_date, start_date, created_at, updated_at) values (4, 2, '광주예술의전당 대극장', '2024-04-11T00:00:00', '2024-04-15T00:00:00', now(), now());
-insert into concert_option (concert_option_id, concert_id, venue, reservation_start_date, start_date, created_at, updated_at) values (5, 2, '고양아람누리 아람극장', '2024-04-11T00:00:00', '2024-04-18T00:00:00', now(), now());
-insert into concert_option (concert_option_id, concert_id, venue, reservation_start_date, start_date, created_at, updated_at) values (6, 3, '경남문화예술회관 대공연장', '2024-04-11T00:00:00', '2024-04-23T00:00:00', now(), now());
-insert into concert_option (concert_option_id, concert_id, venue, reservation_start_date, start_date, created_at, updated_at) values (7, 3, '경기아트센터 대극장', '2024-04-11T00:00:00', '2024-04-14T00:00:00', now(), now());
-insert into concert_option (concert_option_id, concert_id, venue, reservation_start_date, start_date, created_at, updated_at) values (8, 4, '경북대학교 대강당', '2024-04-13T00:00:00', '2024-04-12T00:00:00', now(), now());
+insert into concert_option (concert_option_id, concert_id, venue, reservation_start_date, start_date, created_at, updated_at) values (1, 1, '세종예술의전당', '2024-04-10T00:00:00', '2024-10-15T00:00:00', now(), now());
+insert into concert_option (concert_option_id, concert_id, venue, reservation_start_date, start_date, created_at, updated_at) values (2, 1, '건국대학교 새천년관 대공연장', '2024-04-11T00:00:00', '2024-10-16T00:00:00', now(), now());
+insert into concert_option (concert_option_id, concert_id, venue, reservation_start_date, start_date, created_at, updated_at) values (3, 1, '컨벤션센터 2전시장', '2024-04-08T00:00:00', '2024-10-10T00:00:00', now(), now());
+insert into concert_option (concert_option_id, concert_id, venue, reservation_start_date, start_date, created_at, updated_at) values (4, 2, '광주예술의전당 대극장', '2024-04-11T00:00:00', '2024-10-15T00:00:00', now(), now());
+insert into concert_option (concert_option_id, concert_id, venue, reservation_start_date, start_date, created_at, updated_at) values (5, 2, '고양아람누리 아람극장', '2024-04-11T00:00:00', '2024-10-18T00:00:00', now(), now());
+insert into concert_option (concert_option_id, concert_id, venue, reservation_start_date, start_date, created_at, updated_at) values (6, 3, '경남문화예술회관 대공연장', '2024-04-11T00:00:00', '2024-10-23T00:00:00', now(), now());
+insert into concert_option (concert_option_id, concert_id, venue, reservation_start_date, start_date, created_at, updated_at) values (7, 3, '경기아트센터 대극장', '2024-04-11T00:00:00', '2024-10-14T00:00:00', now(), now());
+insert into concert_option (concert_option_id, concert_id, venue, reservation_start_date, start_date, created_at, updated_at) values (8, 4, '경북대학교 대강당', '2024-04-13T00:00:00', '2024-10-12T00:00:00', now(), now());
 
 INSERT INTO concert_seat (concert_option_id, status, price, seat_no, created_at, updated_at) VALUES (1, 'TEMPORARY', 1000, 1, now(), DATEADD(MINUTE, -2, NOW()));
 INSERT INTO concert_seat (concert_option_id, status, price, seat_no, created_at, updated_at) VALUES (1, 'TEMPORARY', 1000, 2, now(), DATEADD(MINUTE, -1, NOW()));
